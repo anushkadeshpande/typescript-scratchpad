b4c76f4b87555337be13d861bb188f12f1f3bb35
anushkadeshpande <anushkaa5000@gmail.com>
2023-03-14 22:50:30 +0530

    creating own object types

---
+++ src/Data.ts
@@ -0,0 +1,19 @@
+const getData = () => {
+    return [
+        {
+            id: 1,
+            data: 'A'
+        },
+        {
+            id: 2,
+            data: 'B'
+        },
+        {
+            id: 3,
+            data: 'C'
+        }   
+    ]
+}
+
+
+export { getData }
--- src/Post.tsx
+++ src/Post.tsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
+import { getData } from './Data'
+
+type PostProps = {
+    id: number
+    data: String
+}[]
 
 const Post = () => {
-
     const [ postTitle, setPostTitle ] = useState<string | null>("")
-
-    setPostTitle(null)
+    const data: PostProps = getData()
   return (
-    <div>Post</div>
+    <div>{
+        data.map(({id, data}) => <p key={id}>{id} : {data}</p>)
+        }</div>
   )
 }
 
