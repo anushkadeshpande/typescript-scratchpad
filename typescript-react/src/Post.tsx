aa6b009e0a3d6e5cc84820151d5b9bbb579d522f
anushkadeshpande <anushkaa5000@gmail.com>
2023-11-19 03:36:17 +0530

    refactor: Moved old code to separate folder

---
+++ typescript-react/src/Post.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { getData } from "./Data";
+
+type PostProps = {
+  id: number;
+  data: String;
+}[];
+
+const Post = () => {
+  const [postTitle, setPostTitle] = useState<string | null>("");
+  const data: PostProps = getData();
+  return (
+    <div>
+      {data.map(({ id, data }) => (
+        <p key={id}>
+          {id} : {data}
+        </p>
+      ))}
+    </div>  
+  );
+};
+
+export default Post;
